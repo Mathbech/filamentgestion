5bbc785e80b36f04497c780ef5ba06eca21cdc26
Mathbech <legeek.you@gmail.com>
2024-04-23 17:10:42 +0200

    Modification du rendu de la page de détail

--- lib/template/dashboard/detail.dart
+++ lib/template/dashboard/detail.dart
@@ -3,17 +3,37 @@ import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
   final Map<String, dynamic> item;
+  final String detailTitle;
 
-  DetailPage({required String itemString}) : item = jsonDecode(itemString);
+  DetailPage({required String itemString, required String detail}) : item = jsonDecode(itemString), detailTitle = detail;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Détails'),
+        title: Text('${detailTitle}'),
       ),
-      body: Center(
-        child: Text('Détails pour ${item['prix']}'), // Remplacez 'name' par la clé appropriée de votre Map
+    //   body: Center(
+    //     child: Text('Détails pour ${item['prix']}'), // Remplacez 'name' par la clé appropriée de votre Map
+    //   ),
+    // );
+    body: SingleChildScrollView(
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text('Clé'),
+            ),
+            DataColumn(
+              label: Text('Valeur'),
+            ),
+          ],
+          rows: item.keys
+              .map((key) => DataRow(cells: [
+                    DataCell(Text(key)),
+                    DataCell(Text(item[key].toString())),
+                  ]))
+              .toList(),
+        ),
       ),
     );
   }
