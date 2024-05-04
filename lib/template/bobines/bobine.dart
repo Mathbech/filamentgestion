6966faf8ad40b0a3ccbe0f6299108650e78f51b1
Mathbech <legeek.you@gmail.com>
2024-05-04 12:05:03 +0200

    Centralisation de la liste des item sur la page pour une meilleur gestion du rafraichissement

--- lib/template/bobines/bobine.dart
+++ lib/template/bobines/bobine.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../services/api.dart';
 import '../widget/appbar.dart';
-import './list.dart';
+import '../dashboard/detail.dart';
 
 class BobinePage extends StatefulWidget {
   const BobinePage({super.key});
@@ -17,6 +17,8 @@ class BobinePage extends StatefulWidget {
 
 class BobinePageState extends State<BobinePage> {
   List<String> bobines = [];
+  String contentName = 'Bobine';
+  String details = 'Détails de la bobine';
   @override
   void initState() {
     super.initState();
@@ -37,26 +39,100 @@ class BobinePageState extends State<BobinePage> {
     }
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: CustomAppBar(title: 'Stocks'),
+  //     drawer: CustomDrawer(),
+  //     body: FutureBuilder(
+  //       future: getBobines(),
+  //       builder: (context, snapshot) {
+  //         if (snapshot.connectionState == ConnectionState.waiting) {
+  //           return Center(child: CircularProgressIndicator());
+  //         } else if (snapshot.hasError) {
+  //           return Text('Erreur: ${snapshot.error}');
+  //         } else {
+  //           return ListView.builder(
+  //             itemCount: bobines.length,
+  //             itemBuilder: (context, index) {
+  //               return Card(
+  //                 elevation: 3,
+  //                 margin:
+  //                     EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+  //                 child: ListTile(
+  //                   title: Text('${contentName} ${index + 1}'),
+  //                   subtitle: Text('Référence: ${bobines[index]}'),
+  //                   onTap: () {
+  //                     Navigator.push(
+  //                       context,
+  //                       MaterialPageRoute(
+  //                         builder: (context) => DetailPage( itemString: bobines[index], detail: '${details}'),
+  //                       ),
+  //                     );
+  //                   },
+  //                 ),
+  //               );
+  //             },
+  //           );
+  //         }
+  //       }
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Stocks'),
       drawer: CustomDrawer(),
-      body: FutureBuilder(
-        future: getBobines(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Erreur: ${snapshot.error}');
-          } else {
-            return CustomListView(
-              contentName: 'Bobine',
-              items: bobines,
-              details: 'Détails de la bobine',
-            );
-          }
-        }
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            getBobines();
+          });
+        },
+        child: FutureBuilder(
+          future: getBobines(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text('Erreur: ${snapshot.error}');
+            } else {
+              return ListView.builder(
+                itemCount: bobines.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 3,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    child: ListTile(
+                      title: Text('${contentName} ${index + 1}'),
+                      subtitle: Text('Référence: ${bobines[index]}'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPage(
+                                itemString: bobines[index],
+                                detail: '${details}'),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            getBobines();
+          });
+        },
+        child: Icon(Icons.refresh),
       ),
     );
   }
