15b37c5ef238ca37bfd9685a7c99831cc8a85129
Mathbech <legeek.you@gmail.com>
2024-05-04 14:18:10 +0200

    Modification de la page des impressions pour afficher certaines données proprement

--- lib/template/impressions/impressions.dart
+++ lib/template/impressions/impressions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../services/api.dart';
 import '../widget/appbar.dart';
-import '../widget/custom_list_view.dart';
+import 'dart:convert';
 
 class ImpressionsPage extends StatefulWidget {
   const ImpressionsPage({super.key});
@@ -15,7 +15,9 @@ class ImpressionsPage extends StatefulWidget {
 }
 
 class ImpressionsPageState extends State<ImpressionsPage> {
-  List<String> impressions = [];
+  List<Impression> impression = [];
+  String contentName = 'Impression';
+  String details = 'Détails des impresssions';
 
   @override
   void initState() {
@@ -23,18 +25,17 @@ class ImpressionsPageState extends State<ImpressionsPage> {
     getImpressions();
   }
 
-  Future<void> getImpressions() async {
+  Future<List<Impression>> getImpressions() async {
     Api apiInstance = Api();
     List<dynamic>? tempImpressions = await apiInstance.impressions(context);
     if (tempImpressions != null) {
-      impressions =
-          tempImpressions.map((impression) => impression.toString()).toList();
-      print(impressions);
+      impression = tempImpressions.map((impression) => Impression.fromMap(jsonDecode(impression))).toList();
+      return impression;
     } else {
       if (kDebugMode) {
-        print('La méthode impressions a retourné null');
+        print('La méthode bobine a retourné null');
       }
-      return null;
+      return [];
     }
   }
 
@@ -43,22 +44,73 @@ class ImpressionsPageState extends State<ImpressionsPage> {
     return Scaffold(
       appBar: CustomAppBar(title: 'Impressions'),
       drawer: CustomDrawer(),
-      body: FutureBuilder(
-        future: getImpressions(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Erreur: ${snapshot.error}');
-          } else {
-            return CustomListView(
-              contentName: 'Impression',
-              items: impressions,
-              details: 'Détails de l\'impression',
-            );
-          }
+      body: RefreshIndicator(
+          onRefresh: () async {
+            setState(() {
+              getImpressions();
+            });
+          },
+          child: FutureBuilder<List<Impression>>(
+            future: getImpressions(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Erreur: ${snapshot.error}');
+              } else {
+                impression = snapshot.data!;
+                return ListView.builder(
+                  itemCount: impression.length,
+                  itemBuilder: (context, index) {
+                    var impressions = impression[index];
+                    return Card(
+                      elevation: 3,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      child: ListTile(
+                        title: Text('${contentName} ${index + 1}'),
+                        subtitle: Text(
+                            'Poids de bobine consommé: ${impressions.poids}, Date d\'impression: ${impressions.date_impression}'),
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => DetailPage(
+                          //       item: bobine[index],
+                          //       detailTitle: '${details}',
+                          //     ),
+                          //   ),
+                          // );
+                        },
+                      ),
+                    );
+                  },
+                );
+              }
+            },
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            getImpressions();
+          });
         },
+        child: Icon(Icons.refresh),
       ),
+    );
+  }
+}
+
+class Impression {
+  final double poids;
+  final DateTime date_impression;
+
+  Impression({required this.poids, required this.date_impression});
+
+  factory Impression.fromMap(Map<String, dynamic> map) {
+    return Impression(
+      poids: map['poids'],
+      date_impression: DateTime.parse(map['date']),
     );
   }
 }
