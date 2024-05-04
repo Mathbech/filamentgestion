ac22272bcc769ec3297f6634fac286c319e806d9
Mathbech <legeek.you@gmail.com>
2024-05-04 12:43:39 +0200

    Modification des détails affiché dans la liste

--- lib/template/bobines/bobine.dart
+++ lib/template/bobines/bobine.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../services/api.dart';
 import '../widget/appbar.dart';
 import '../dashboard/detail.dart';
+import 'dart:convert';
 
 class BobinePage extends StatefulWidget {
   const BobinePage({super.key});
@@ -16,7 +17,7 @@ class BobinePage extends StatefulWidget {
 }
 
 class BobinePageState extends State<BobinePage> {
-  List<String> bobines = [];
+  List<Bobine> bobine = [];
   String contentName = 'Bobine';
   String details = 'Détails de la bobine';
   @override
@@ -25,60 +26,20 @@ class BobinePageState extends State<BobinePage> {
     getBobines();
   }
 
-  Future<void> getBobines() async {
-    Api apiInstance = Api();
-    List<dynamic>? tempBobines = await apiInstance.bobine(context);
-    if (tempBobines != null) {
-      bobines = tempBobines.map((bobine) => bobine.toString()).toList();
-      print(bobines);
-    } else {
-      if (kDebugMode) {
-        print('La méthode bobine a retourné null');
-      }
-      return null;
+Future<List<Bobine>> getBobines() async {
+  Api apiInstance = Api();
+  List<dynamic>? tempBobines = await apiInstance.bobine(context);
+  if (tempBobines != null) {
+    bobine = tempBobines.map((bobine) => Bobine.fromMap(jsonDecode(bobine))).toList();
+    return bobine;
+  } else {
+    if (kDebugMode) {
+      print('La méthode bobine a retourné null');
     }
+    return [];
   }
+}
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: CustomAppBar(title: 'Stocks'),
-  //     drawer: CustomDrawer(),
-  //     body: FutureBuilder(
-  //       future: getBobines(),
-  //       builder: (context, snapshot) {
-  //         if (snapshot.connectionState == ConnectionState.waiting) {
-  //           return Center(child: CircularProgressIndicator());
-  //         } else if (snapshot.hasError) {
-  //           return Text('Erreur: ${snapshot.error}');
-  //         } else {
-  //           return ListView.builder(
-  //             itemCount: bobines.length,
-  //             itemBuilder: (context, index) {
-  //               return Card(
-  //                 elevation: 3,
-  //                 margin:
-  //                     EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-  //                 child: ListTile(
-  //                   title: Text('${contentName} ${index + 1}'),
-  //                   subtitle: Text('Référence: ${bobines[index]}'),
-  //                   onTap: () {
-  //                     Navigator.push(
-  //                       context,
-  //                       MaterialPageRoute(
-  //                         builder: (context) => DetailPage( itemString: bobines[index], detail: '${details}'),
-  //                       ),
-  //                     );
-  //                   },
-  //                 ),
-  //               );
-  //             },
-  //           );
-  //         }
-  //       }
-  //     ),
-  //   );
-  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,41 +51,45 @@ class BobinePageState extends State<BobinePage> {
             getBobines();
           });
         },
-        child: FutureBuilder(
-          future: getBobines(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Text('Erreur: ${snapshot.error}');
-            } else {
-              return ListView.builder(
-                itemCount: bobines.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    elevation: 3,
-                    margin:
-                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                    child: ListTile(
-                      title: Text('${contentName} ${index + 1}'),
-                      subtitle: Text('Référence: ${bobines[index]}'),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailPage(
-                                itemString: bobines[index],
-                                detail: '${details}'),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-              );
-            }
-          },
-        ),
+        child: FutureBuilder<List<Bobine>>(
+            future: getBobines(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Erreur: ${snapshot.error}');
+              } else {
+                bobine = snapshot.data!;
+                return ListView.builder(
+                  itemCount: bobine.length,
+                  itemBuilder: (context, index) {
+                    var bobines = bobine[index];
+                    return Card(
+                      elevation: 3,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      child: ListTile(
+                        title: Text('${contentName} ${index + 1}'),
+                        subtitle: Text(
+                            'Couleur: ${bobines.couleur}, Catégorie de filament: ${bobines.categorie}'),
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => DetailPage(
+                          //       item: bobine[index],
+                          //       detailTitle: '${details}',
+                          //     ),
+                          //   ),
+                          // );
+                        },
+                      ),
+                    );
+                  },
+                );
+              }
+            },
+          )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -134,6 +99,21 @@ class BobinePageState extends State<BobinePage> {
         },
         child: Icon(Icons.refresh),
       ),
+    );
+  }
+}
+
+class Bobine {
+  final String couleur;
+  final String categorie;
+
+  Bobine({required this.couleur, required this.categorie});
+
+  // Convertir un Map en une instance de Bobine
+  factory Bobine.fromMap(Map<String, dynamic> map) {
+    return Bobine(
+      couleur: map['couleur'],
+      categorie: map['categorie'],
     );
   }
 }
