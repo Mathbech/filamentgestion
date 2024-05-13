8f4c65e096dd6ff96c1958e1bba581fc8e2829b1
Mathbech <legeek.you@gmail.com>
2024-05-13 11:18:47 +0200

    Modification de la taille du tableau de détail + ajout d'une dépendances pour

--- lib/template/dashboard/detail.dart
+++ lib/template/dashboard/detail.dart
@@ -1,4 +1,40 @@
+// import 'package:flutter/material.dart';
+
+// class DetailPage extends StatelessWidget {
+//   final Map<String, dynamic> item;
+//   final String detailTitle;
+//   DetailPage({required this.item, required this.detailTitle});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('${detailTitle}'),
+//       ),
+//       body: SingleChildScrollView(
+//         child: DataTable(
+//           columns: const <DataColumn>[
+//             DataColumn(
+//               label: Text('Clé'),
+//             ),
+//             DataColumn(
+//               label: Text('Valeur'),
+//             ),
+//           ],
+//           rows: item.keys
+//               .map((key) => DataRow(cells: [
+//                     DataCell(Text(key)),
+//                     DataCell(Text(item[key].toString())),
+//                   ]))
+//               .toList(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class DetailPage extends StatelessWidget {
   final Map<String, dynamic> item;
@@ -23,8 +59,20 @@ class DetailPage extends StatelessWidget {
           ],
           rows: item.keys
               .map((key) => DataRow(cells: [
-                    DataCell(Text(key)),
-                    DataCell(Text(item[key].toString())),
+                    DataCell(
+                      Container(
+                        width:
+                            100, // Ajustez cette valeur en fonction de vos besoins
+                        child: AutoSizeText(key),
+                      ),
+                    ),
+                    DataCell(
+                      Container(
+                        width:
+                            100, // Ajustez cette valeur en fonction de vos besoins
+                        child: AutoSizeText(item[key].toString()),
+                      ),
+                    ),
                   ]))
               .toList(),
         ),
