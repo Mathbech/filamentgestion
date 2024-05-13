d42ce26c1753842e8812063a2af55a076aac8db9
Mathbech <legeek.you@gmail.com>
2024-05-13 12:02:26 +0200

    Modification de la taille du tableau et du texte dans le tableau

--- lib/template/dashboard/detail.dart
+++ lib/template/dashboard/detail.dart
@@ -1,38 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class DetailPage extends StatelessWidget {
-//   final Map<String, dynamic> item;
-//   final String detailTitle;
-//   DetailPage({required this.item, required this.detailTitle});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('${detailTitle}'),
-//       ),
-//       body: SingleChildScrollView(
-//         child: DataTable(
-//           columns: const <DataColumn>[
-//             DataColumn(
-//               label: Text('Clé'),
-//             ),
-//             DataColumn(
-//               label: Text('Valeur'),
-//             ),
-//           ],
-//           rows: item.keys
-//               .map((key) => DataRow(cells: [
-//                     DataCell(Text(key)),
-//                     DataCell(Text(item[key].toString())),
-//                   ]))
-//               .toList(),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
@@ -61,15 +26,13 @@ class DetailPage extends StatelessWidget {
               .map((key) => DataRow(cells: [
                     DataCell(
                       Container(
-                        width:
-                            100, // Ajustez cette valeur en fonction de vos besoins
+                        width: 100, // Ajustez cette valeur en fonction de vos besoins
                         child: AutoSizeText(key),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width:
-                            100, // Ajustez cette valeur en fonction de vos besoins
+                        width: 100, // Ajustez cette valeur en fonction de vos besoins
                         child: AutoSizeText(item[key].toString()),
                       ),
                     ),
